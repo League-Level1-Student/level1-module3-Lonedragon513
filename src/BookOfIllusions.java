b1fb7b1c3ea85767f57987901c03205ac3d6be67
league <league@wts8.attlocal.net>
2018-10-17 19:59:59 -0700

    gsd

--- src/BookOfIllusions.java
+++ src/BookOfIllusions.java
@@ -4,17 +4,20 @@
  *    Level 1
  */
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
  
 /** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
 
-public class BookOfIllusions extends MouseAdapter {
+public class BookOfIllusions extends MouseAdapter  {
 
 	/*
 	 * Here we are instantiating our BookOfIllusions class and calling it’s createBook() method. This is because we want to get out of the
@@ -27,24 +30,39 @@ public class BookOfIllusions extends MouseAdapter {
 	}
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+	JFrame f = new JFrame();
 	private void createBook() {
 		// 2. make the frame visible
+		f.setVisible(true);
 		// 3. set the size of the frame
+		f.setSize(500, 500);
 		// 4. find 2 images and save them to your project’s default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		String life="life.jpg";
+		
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		JLabel l;
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+		l = loadImageFromComputer(life);
 		// 8. add your JLabel to the frame
+		f.add(l);
 		// 9. call the pack() method on the frame
+		f.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		f.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("clicked!!!!!!!!!!");
 		// 12. remove everything from the frame that was added earlier
+		f.removeAll();;
 		// 13. load a new image like before (this is more than one line of code)
+		JLabel ll;
+		String ft="left.jpg";
+		ll = loadImageFromComputer(ft);
 		// 14. pack the frame
+		f.pack();
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
@@ -59,5 +77,3 @@ public class BookOfIllusions extends MouseAdapter {
 	}
 
 }
-
-
--- src/TT.java
+++ src/TT.java
@@ -1,29 +1,73 @@
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class TT {
-public static void main(String[] args) {
+public class TT implements KeyListener{
 	
-	TT t = new TT();
-	}
-
-
-void t () {
-	JFrame f = new JFrame ();
-	f.setVisible(true);
-	f.setTitle("Type or Die");
 	
 	char currentLetter;
+public static void main(String[] args) {
+	TT tt = new TT();
+	tt.t();
+	}
+
+JFrame f = new JFrame ();
+JLabel l = new JLabel();
+
+void t () {
+	f.setVisible(true);
+	f.setTitle("Type or Die");
+	i();
+	f.addKeyListener(this);
+}
+
+void i () {
+	
 
 	currentLetter = generateRandomLetter();
-
-	JLabel l = new JLabel();
+	
+	
+	l.setText(""+currentLetter);
+	l.setFont(l.getFont().deriveFont(28.0f));
+	l.setHorizontalAlignment(JLabel.CENTER);
+	f.add(l);
+	f.pack();
 	}
 
 char generateRandomLetter() {
 	      Random r = new Random();
 	      return (char) (r.nextInt(26) + 'a');
+}
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	if(e.getKeyChar()==currentLetter) {
+		System.out.println("correct");
+		System.out.println("u typed " + currentLetter);
+		f.setBackground(Color.green);
+	}
+	else {
+		System.out.println("wrong");
+		System.out.println("u typed " + currentLetter);
+		f.setBackground(Color.red);
+	}
+	f.remove(l);
+	i();
+
+	
+}
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
 }
 }
